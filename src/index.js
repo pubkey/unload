1e74c4bddd0b971a185bdfe076d9d3ecf28d54b1
Daniel M <daniel_van_meyer@hotmail.de>
2016-12-08 16:35:44 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,27 @@
 
+
+var envs={
+  node: require('./nodeJS.js'),
+  browser: require('./browser.js)
+};
+          
+          
+/**
+ * start listening with the handler
+ * @param  {Function({})} fn the handler
+ * @return {Function} a function which is used to stop listening
+ */
+module.exports = function unload(fn){
+  var hasListeners = {};
+  Object.keys(envs).forEach(function(envKey){
+    hasListeners[envKey] = envs[envKey].add(fn); 
+  });
+
+  var retFn = function stopListening(){
+    Object.keys(hasListeners).forEach(function(envKey){
+      envs[envKey].remove(fn);
+    });    
+  }
+
+}
+
