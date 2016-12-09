7ab61d9ad3f429d1373c3082e84ce3e05c74a76b
pubkey <daniel_van_meyer@hotmail.de>
2016-12-09 01:10:53 +0100

    BUILD

---
+++ dist/unload.js
@@ -0,0 +1,329 @@
+(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
+// shim for using process in browser
+var process = module.exports = {};
+
+// cached from whatever global is present so that test runners that stub it
+// don't break things.  But we need to wrap it in a try catch in case it is
+// wrapped in strict mode code which doesn't define any globals.  It's inside a
+// function because try/catches deoptimize in certain engines.
+
+var cachedSetTimeout;
+var cachedClearTimeout;
+
+function defaultSetTimout() {
+    throw new Error('setTimeout has not been defined');
+}
+function defaultClearTimeout () {
+    throw new Error('clearTimeout has not been defined');
+}
+(function () {
+    try {
+        if (typeof setTimeout === 'function') {
+            cachedSetTimeout = setTimeout;
+        } else {
+            cachedSetTimeout = defaultSetTimout;
+        }
+    } catch (e) {
+        cachedSetTimeout = defaultSetTimout;
+    }
+    try {
+        if (typeof clearTimeout === 'function') {
+            cachedClearTimeout = clearTimeout;
+        } else {
+            cachedClearTimeout = defaultClearTimeout;
+        }
+    } catch (e) {
+        cachedClearTimeout = defaultClearTimeout;
+    }
+} ())
+function runTimeout(fun) {
+    if (cachedSetTimeout === setTimeout) {
+        //normal enviroments in sane situations
+        return setTimeout(fun, 0);
+    }
+    // if setTimeout wasn't available but was latter defined
+    if ((cachedSetTimeout === defaultSetTimout || !cachedSetTimeout) && setTimeout) {
+        cachedSetTimeout = setTimeout;
+        return setTimeout(fun, 0);
+    }
+    try {
+        // when when somebody has screwed with setTimeout but no I.E. maddness
+        return cachedSetTimeout(fun, 0);
+    } catch(e){
+        try {
+            // When we are in I.E. but the script has been evaled so I.E. doesn't trust the global object when called normally
+            return cachedSetTimeout.call(null, fun, 0);
+        } catch(e){
+            // same as above but when it's a version of I.E. that must have the global object for 'this', hopfully our context correct otherwise it will throw a global error
+            return cachedSetTimeout.call(this, fun, 0);
+        }
+    }
+
+
+}
+function runClearTimeout(marker) {
+    if (cachedClearTimeout === clearTimeout) {
+        //normal enviroments in sane situations
+        return clearTimeout(marker);
+    }
+    // if clearTimeout wasn't available but was latter defined
+    if ((cachedClearTimeout === defaultClearTimeout || !cachedClearTimeout) && clearTimeout) {
+        cachedClearTimeout = clearTimeout;
+        return clearTimeout(marker);
+    }
+    try {
+        // when when somebody has screwed with setTimeout but no I.E. maddness
+        return cachedClearTimeout(marker);
+    } catch (e){
+        try {
+            // When we are in I.E. but the script has been evaled so I.E. doesn't  trust the global object when called normally
+            return cachedClearTimeout.call(null, marker);
+        } catch (e){
+            // same as above but when it's a version of I.E. that must have the global object for 'this', hopfully our context correct otherwise it will throw a global error.
+            // Some versions of I.E. have different rules for clearTimeout vs setTimeout
+            return cachedClearTimeout.call(this, marker);
+        }
+    }
+
+
+
+}
+var queue = [];
+var draining = false;
+var currentQueue;
+var queueIndex = -1;
+
+function cleanUpNextTick() {
+    if (!draining || !currentQueue) {
+        return;
+    }
+    draining = false;
+    if (currentQueue.length) {
+        queue = currentQueue.concat(queue);
+    } else {
+        queueIndex = -1;
+    }
+    if (queue.length) {
+        drainQueue();
+    }
+}
+
+function drainQueue() {
+    if (draining) {
+        return;
+    }
+    var timeout = runTimeout(cleanUpNextTick);
+    draining = true;
+
+    var len = queue.length;
+    while(len) {
+        currentQueue = queue;
+        queue = [];
+        while (++queueIndex < len) {
+            if (currentQueue) {
+                currentQueue[queueIndex].run();
+            }
+        }
+        queueIndex = -1;
+        len = queue.length;
+    }
+    currentQueue = null;
+    draining = false;
+    runClearTimeout(timeout);
+}
+
+process.nextTick = function (fun) {
+    var args = new Array(arguments.length - 1);
+    if (arguments.length > 1) {
+        for (var i = 1; i < arguments.length; i++) {
+            args[i - 1] = arguments[i];
+        }
+    }
+    queue.push(new Item(fun, args));
+    if (queue.length === 1 && !draining) {
+        runTimeout(drainQueue);
+    }
+};
+
+// v8 likes predictible objects
+function Item(fun, array) {
+    this.fun = fun;
+    this.array = array;
+}
+Item.prototype.run = function () {
+    this.fun.apply(null, this.array);
+};
+process.title = 'browser';
+process.browser = true;
+process.env = {};
+process.argv = [];
+process.version = ''; // empty string to avoid regexp issues
+process.versions = {};
+
+function noop() {}
+
+process.on = noop;
+process.addListener = noop;
+process.once = noop;
+process.off = noop;
+process.removeListener = noop;
+process.removeAllListeners = noop;
+process.emit = noop;
+
+process.binding = function (name) {
+    throw new Error('process.binding is not supported');
+};
+
+process.cwd = function () { return '/' };
+process.chdir = function (dir) {
+    throw new Error('process.chdir is not supported');
+};
+process.umask = function() { return 0; };
+
+},{}],2:[function(require,module,exports){
+module.exports = (function() {
+    var exports = {};
+
+    exports.add = function(fn) {
+        var ret = [];
+        if (
+            typeof window === 'object' &&
+            window.addEventListener &&
+            typeof window.addEventListener === 'function'
+        ) {
+            window.addEventListener('beforeunload', fn, false);
+            ret.push('beforeunload');
+        }
+
+        /**
+         * TODO add fallback for safari-mobile
+         * @link http://stackoverflow.com/questions/3239834/window-onbeforeunload-not-working-on-the-ipad
+         */
+
+        return ret;
+    };
+
+    exports.remove = function(fn, listenerKeys) {
+        listenerKeys.forEach(function(key) {
+            switch (key) {
+                case 'beforeunload':
+                    window.removeEventListener('beforeunload',
+                        fn,
+                        false
+                    );
+                    break;
+            }
+        });
+    };
+
+
+    return exports;
+
+})();
+
+},{}],3:[function(require,module,exports){
+var unload = require('./index.js');
+window['unload'] = unload;
+
+},{"./index.js":4}],4:[function(require,module,exports){
+module.exports = (function(
+    envs
+) {
+    var unloaded = false;
+
+    /**
+     * start listening with the handler
+     * @param  {Function({})} fn the handler which takes the unload-event as attr
+     * @return {Function} stopListening : a function which is used to stop listening
+     */
+    var exports = function(fn) {
+
+        // wrap fn to ensure it executes once
+        var fnWrapped = function(arg1, arg2, arg3) {
+            if (unloaded) return;
+            unloaded = true;
+            return fn(arg1, arg2, arg3);
+        };
+
+        var hasListeners = {};
+        Object.keys(envs).forEach(function(envKey) {
+            hasListeners[envKey] = envs[envKey].add(fnWrapped);
+        });
+        var retFn = function stopListening() {
+            Object.keys(hasListeners).forEach(function(envKey) {
+                envs[envKey].remove(fnWrapped, hasListeners[envKey]);
+            });
+        };
+    };
+
+    return exports;
+
+})({
+    node: require('./nodeJS.js'),
+    browser: require('./browser.js')
+});
+
+},{"./browser.js":2,"./nodeJS.js":5}],5:[function(require,module,exports){
+(function (process){
+module.exports = (function() {
+    var exports = {};
+
+    exports.add = function(fn) {
+        var ret = [];
+        if (
+            typeof process === 'object' &&
+            process.on &&
+            typeof process.on === 'function'
+        ) {
+            process.on('beforeExit', function(e) {
+                var maybePromise = fn(e);
+                Promise.resolve(maybePromise)
+                    .then(function() {
+                        process.exit();
+                    });
+            });
+            ret.push('beforeExit');
+
+            //catches ctrl+c event
+            process.on('SIGINT', function(e) {
+                var maybePromise = fn(e);
+                Promise.resolve(maybePromise)
+                    .then(function() {
+                        process.exit();
+                    });
+            });
+            ret.push('SIGINT');
+
+            //catches uncaught exceptions
+            process.on('uncaughtException', function(e) {
+                var maybePromise = fn(e);
+                Promise.resolve(maybePromise)
+                    .then(function() {
+                        process.exit();
+                    });
+            });
+            ret.push('uncaughtException');
+        }
+
+        return ret;
+    };
+
+    exports.remove = function(fn, listenerKeys) {
+        listenerKeys.forEach(function(key) {
+            switch (key) {
+                case 'beforeExit':
+                case 'SIGINT':
+                case 'uncaughtException':
+                    process.removeListener(key, fn);
+                    break;
+            }
+        });
+    };
+
+
+    return exports;
+
+})();
+
+}).call(this,require('_process'))
+},{"_process":1}]},{},[3]);
