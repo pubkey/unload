c40db88c7f889164b70b2ed819df3249d3b2a496
pubkey <daniel_van_meyer@hotmail.de>
2016-12-15 23:05:40 +0100

    FIX lint

--- test/helper/getPingCount.node.js
+++ test/helper/getPingCount.node.js
@@ -2,7 +2,7 @@ module.exports = (function(request) {
     var pingHost = 'http://localhost:23230/counter/';
     return function() {
         return request(pingHost).then(c => parseInt(c));
-    }
+    };
 })(
     require('request-promise-native')
 );
