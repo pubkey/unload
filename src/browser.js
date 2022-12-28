239580626cffa63a0a68e54f38728e0c7e4359e5
pubkey <8926560+pubkey@users.noreply.github.com>
2022-12-28 12:08:33 +0100

    REFACTOR stuff

--- src/browser.js
+++ src/browser.js
@@ -1,15 +1,27 @@
 /* global WorkerGlobalScope */
 
-function add(fn) {
+export function addBrowser(fn) {
     if (typeof WorkerGlobalScope === 'function' && self instanceof WorkerGlobalScope) {
-        // this is run inside of a webworker
+        /**
+         * Because killing a worker does directly stop the excution
+         * of the code, our only chance is to overwrite the close function
+         * which could work some times.
+         * @link https://stackoverflow.com/q/72903255/3443137
+         */
+        const oldClose = self.close.bind(self);
+        self.close = () => {
+            fn();
+            return oldClose();
+        };
     } else {
 
         /**
          * if we are on react-native, there is no window.addEventListener
          * @link https://github.com/pubkey/unload/issues/6
          */
-        if (typeof window.addEventListener !== 'function') return;
+        if (typeof window.addEventListener !== 'function') {
+            return;
+        }
 
         /**
          * for normal browser-windows, we use the beforeunload-event
@@ -32,7 +44,3 @@ function add(fn) {
      * @link https://stackoverflow.com/a/26193516/3443137
      */
 }
-
-export default {
-    add
-};
--- src/node.js
+++ src/node.js
@@ -1,9 +1,5 @@
-// set to true to log events
-const DEBUG = false;
-
-function add(fn) {
+export function addNode(fn) {
     process.on('exit', () => {
-        DEBUG && console.log('node: exit');
         return fn();
     });
 
@@ -14,25 +10,18 @@ function add(fn) {
      * therefore we have to call process.exit()
      */
     process.on('beforeExit', () => {
-        DEBUG && console.log('node: beforeExit');
         return fn().then(() => process.exit());
     });
     // catches ctrl+c event
     process.on('SIGINT', () => {
-        DEBUG && console.log('node: SIGNINT');
         return fn().then(() => process.exit());
     });
     // catches uncaught exceptions
     process.on('uncaughtException', err => {
-        DEBUG && console.log('node: uncaughtException');
         return fn()
             .then(() => {
                 console.trace(err);
-                process.exit(1);
+                process.exit(101);
             });
     });
 }
-
-export default {
-    add
-};
